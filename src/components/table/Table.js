01e2794c64621017381e36530476ff2e87d59786
romvtk <romvtk@mail.ru>
2021-01-12 00:33:40 +0300

    Framework (#3)
    * create listener
    
    * Finish framework functional
    
    Co-authored-by: romvtk <Gia016958&>
    Co-authored-by: ivangricenko <0.ivangricenko.0@gmail.com>

---
+++ src/components/table/Table.js
@@ -0,0 +1,10 @@
+/* eslint-disable require-jsdoc */
+import { ExcelComponent } from '@core/ExcelComponent';
+import { createTable } from './table.template';
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table';
+  toHTML() {
+    return createTable(20);
+  }
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,20 @@
+/* eslint-disable require-jsdoc */
+import { DomListener } from './DomListener';
+
+export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+    this.name = options.name || '';
+  }
+  toHTML() {
+    return '';
+  }
+
+  init() {
+    this.initDOMListeners();
+  }
+
+  destroy() {
+    this.removeDOMListeners();
+  }
+}
