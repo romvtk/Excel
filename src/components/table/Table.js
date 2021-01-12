e64023f47646abf2f35a9d5084b9ce3c8c01a6ed
ivangricenko <0.ivangricenko.0@gmail.com>
2021-01-13 01:42:20 +0300

    top resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,44 @@
 /* eslint-disable require-jsdoc */
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
+import { $ } from './../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(20);
   }
+
+  // onClick() {
+  //   console.log('click');
+  // }
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target);
+      // const $parent = $resizer.$el.closest('.column');
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coords = $parent.getCoords();
+      document.onmousemove = (e) => {
+        const delta = e.pageX - coords.right;
+        const value = coords.width + delta;
+        $parent.$el.style.width = value + 'px';
+      };
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
+    }
+  }
+  // onMousemove() {
+  //   console.log('mousemove');
+  // }
+  // onMouseup() {
+  //   console.log('mouseup');
+  // }
 }
